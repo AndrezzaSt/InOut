5d6c8b59e57ce59984edf004e6a5cfdc21e83fca
Andrezza Sturm <Andrezza.sturm@hotmail.com>
2021-08-25 17:21:35 -0300

    Getting Users

--- public/index.php
+++ public/index.php
@@ -2,4 +2,11 @@
     require_once(dirname(__FILE__, 2) . '/src/config/config.php' );
     require_once(dirname(__FILE__, 2) . '/src/models/User.php' );
     
-$user = new User();
+
+
+print_r(User::get(['id' => 1], 'id, name, email'));
+
+foreach(User::get([], 'name') as $user) {
+    echo $user->name;
+    echo '<br>';
+}
