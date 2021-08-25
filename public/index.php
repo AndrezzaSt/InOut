320d574527e31910cb9592658fe5537da599278d
Andrezza Sturm <Andrezza.sturm@hotmail.com>
2021-08-25 16:37:35 -0300

    Class User

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
-    require_once(dirname(__FILE__, 2) . '/src/config/database.php' );
-
-    $sql = 'select * from users';
-    Database::getResultFromQuery($sql);
-
-    while($row = $result->fetch_assoc()){
-        print_r($row);
-        echo '<br>';
-    }
+    require_once(dirname(__FILE__, 2) . '/src/config/config.php' );
+    require_once(dirname(__FILE__, 2) . '/src/models/User.php' );
+    
+$user = new User();
