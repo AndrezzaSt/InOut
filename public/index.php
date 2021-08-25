64910afe138f73ea09141d4fc9e2fa22b98b9964
Andrezza Sturm <Andrezza.sturm@hotmail.com>
2021-08-25 13:41:09 -0300

    Database Class

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
-<h1>
-    
-</h1>
+<?php
+    require_once(dirname(__FILE__, 2) . '/src/config/database.php' );
+
+    $sql = 'select * from users';
+    Database::getResultFromQuery($sql);
+
+    while($row = $result->fetch_assoc()){
+        print_r($row);
+        echo '<br>';
+    }
