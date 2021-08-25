09c94a9c49a7004fbd74f105a5360a7357678c44
Andrezza Sturm <Andrezza.sturm@hotmail.com>
2021-08-25 17:01:29 -0300

    Generating Select Command

--- src/models/model.php
+++ src/models/model.php
@@ -24,4 +24,31 @@
         public function __set($key, $value) {
             $this->values[$key] = $value;
         }
+
+        public static function getSelect($filters = [], $columns = '*') {
+            $sql ='SELECT ${columns} from '
+            . static::$tableName
+            . static::getFilters($filters);
+            return $sql;
+        }
+
+        private static function getFilters($filters) {
+            $sql = '';
+            if(count($filters) > 0) {
+                $sql .= "Where 1 = 1";
+                foreach($filters as $column => $value) {
+                    $sql .= " AND ${column} = " . static::getFormatedValue($value);
+                }
+            }
+            return $sql;  
+        }
+
+        private static function getFormatedValue($value){
+            if(is_null($value)){
+                return "null";
+            }elseif(gettype($value) === 'string') {
+                return "'{$value}'";
+            }else
+                return $value;
+        }
     }
