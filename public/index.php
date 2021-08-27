0f3ddb6ad9f976ed17a9701f117556a85ed6c3fa
Andrezza Sturm <Andrezza.sturm@hotmail.com>
2021-08-27 14:04:24 -0300

    Redirecting

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
-    require_once(dirname(__FILE__, 2) . '/src/config/config.php' );
-    require_once(CONTROLLER_PATH. '/src/models/login.php' );
+require_once(dirname(__FILE__, 2) . '/src/config/config.php' );
+
+$uri= urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
     
-loadView('login');
+if($uri === '/' || $uri === '' || $uri === '/index.php') {
+    $uri = '/login.php';
+}
+
+require_once(CONTROLLER_PATH . "/{$uri}" );
